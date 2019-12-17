6ecb9f648ca5c9ad1722bfe7ee837e411a5a741a
Giorgos Kostomanolakis <csd3787@csd.uoc.gr>
2019-12-17 03:57:52 +0200

    git ignore works

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     vector<string> msg{"Hello", "C++", "World", "from", "VS Code!"};
-
+    // this is a comment
     for (const string& word : msg) {
         cout << word << " ";
     }
